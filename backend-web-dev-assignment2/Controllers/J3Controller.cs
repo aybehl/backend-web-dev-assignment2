27b2c437c3f035dceb45dcf1acc2da673443c36b
Ayushi Behl <ayushi.behl94@gmail.com>
2024-02-11 21:58:58 -0800

    Solved the J3 problem and did local testing

--- backend-web-dev-assignment2/Controllers/J3Controller.cs
+++ backend-web-dev-assignment2/Controllers/J3Controller.cs
@@ -10,31 +10,61 @@ namespace backend_web_dev_assignment2.Controllers
 {
     public class J3Controller : ApiController
     {
-        private Boolean isInvalidInstruction(Char c) {
-            if ((c >= 'A' && c <= 'T') || (c == '+') || (c == '-')) {
-                return false;
-            }
-
-            return true;
-        }
-
-
         [Route("api/J3/harpTuning/{instructions}")]
         [HttpGet]
-        public  IEnumerable<string> harpTuning(string instructions)
+        public IEnumerable<string> harpTuning(string instructions)
         {
             List<string> listOfTuningInstructions = new List<string>();
             int lenOfInstructions = instructions.Length;
             StringBuilder tempStr = new StringBuilder();
             int i = 0;
+            Boolean hasSeenANumber = false;
             while (i < lenOfInstructions) {
                 char c = instructions[i];
-                if (isInvalidInstruction(c)) { 
-                    return new List<string>();
-                }
-                //todo
 
+                if (c >= 'A' && c <= 'T')
+                {
+                    if (!hasSeenANumber)
+                    {
+                        tempStr.Append(c);
+                    }
+                    else {
+                        listOfTuningInstructions.Add(tempStr.ToString());
+                        hasSeenANumber = false;
+                        tempStr.Clear();
+                        tempStr.Append(c);
+                    }
+                }
+                else if (c >= '0' && c <= '9')
+                {
+                    tempStr.Append(c);
+                    hasSeenANumber = true;
+                }
+                else if (c == '+' || c == '-')
+                {
+                    tempStr.Append(" ");
+                    if (c == '+')
+                    {
+                        tempStr.Append("tighten");
+                    }
+                    else {
+                        tempStr.Append("loosen");
+                    }
+                    tempStr.Append(" ");
+                }
+                else {
+                    return new string[] {"Invalid characters in instructions"};
+                }
+
+                i++;
             }
+
+            if(hasSeenANumber)
+            {
+                listOfTuningInstructions.Add(tempStr.ToString());
+                tempStr.Clear();
+            }
+
             return listOfTuningInstructions;
         }
     }
